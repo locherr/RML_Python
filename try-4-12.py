6e3b486226cdb2357dc9319bd39da935bed48627
locherr <rita.locher@bluemail.ch>
2025-05-08 12:19:38 +0200

    code for exercise try-4-13

---
+++ try-4-12.py
@@ -0,0 +1,8 @@
+foods =["Baumstrünke", "Spaghetti mit Crevetten", "Sushi", "Knöpfli"]
+my_friends_foods = ["Bratwürste", "Kartoffelstock", "Schoggi"]
+
+for food in foods:
+    print(food)
+
+for food in my_friends_foods:
+    print(food)
