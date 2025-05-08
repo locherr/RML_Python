f4348659a36565c23998285fdc00e2fe497062b1
locherr <rita.locher@bluemail.ch>
2025-05-08 12:17:54 +0200

    code for exercise 4-10

---
+++ try-4-10.py
@@ -0,0 +1,5 @@
+reptiles = ["Kreuzotter", "Eidechse", "Ringelnatter", "Krokodil", "Blindschleiche"]
+
+print(f" The first three items in the list are {reptiles[0:3]}")
+print(f" Three reptiles from the middle of the list are {reptiles[1:-1]}")
+print(f" The last three reptiles in the list are {reptiles[-3:]}")
